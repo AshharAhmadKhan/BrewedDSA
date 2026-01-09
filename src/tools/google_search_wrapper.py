ec0e738e7e0a978f48abfc29421b63a56f6f40b8
Ashhar Ahmad Khan <145142826+AshharAhmadKhan@users.noreply.github.com>
2026-01-09 13:50:41 +0530

    Clear all content from repo

--- src/tools/google_search_wrapper.py
+++
@@ -1,8 +0,0 @@
-class GoogleSearchTool:
-    """
-    Placeholder for built-in Google Search tool.
-    """
-
-    def search(self, query: str) -> list:
-        # TODO: Replace with actual tool call in ADK version
-        return []
